5dad6b1ba03b9b6ed8e996268c3a205a3e24585a
gaowei0115 <gao_wei0115@sina.com>
2017-06-29 22:48:44 +0800

    spring-boot-simple初始简单boot工程

---
+++ spring-boot-simple/src/main/java/com/mmc/boot/system/simple/Application.java
@@ -0,0 +1,27 @@
+// Copyright (C) 2016-2017 GWGW All rights reserved
+package com.mmc.boot.system.simple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/** 
+ * ClassName: Application<br/>
+ * Description: <br/>
+ * Author: GW<br/>
+ * Create： 2017年6月29日<br/>
+ *
+ * History: (Version) Author dateTime description <br/>
+ */
+@SpringBootApplication
+public class Application {
+
+	/**
+	 * Description：启动入口<br/>
+	 * Author：GW<br/>
+	 * History: (Version) Author dateTime description <br/>
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
